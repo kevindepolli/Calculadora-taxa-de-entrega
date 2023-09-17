1db7fb1c9b8e7b2e9e3a271fb1ac91dfb8d0fd45
Kevin Depolli <kevindepollidcf@gmail.com>
2023-09-17 16:31:05 -0300

    Create a ICalculadoraTaxaEntrega implementation CalculadoraTaxaEntregaPadrao

---
+++ src/main/java/com/ufes/CalculadoraTaxaEntregaPadrao.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufes;
+
+/**
+ *
+ * @author Kevin
+ */
+public class CalculadoraTaxaEntregaPadrao {
+    private double fatorPadrao;
+
+    public CalculadoraTaxaEntregaPadrao(double fatorPadrao) {
+        this.fatorPadrao = fatorPadrao;
+    }
+
+    public double getFatorPadrao() {
+        return fatorPadrao;
+    }
+
+    public void setFatorPadrao(double fatorPadrao) {
+        this.fatorPadrao = fatorPadrao;
+    }
+    
+    public void calcularTaxaEntrega(Pedido pedido){
+        if(pedido.getTaxaEntrega() == -1)
+            pedido.setTaxaEntrega(fatorPadrao);
+    }
+}
