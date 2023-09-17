618781d95b187e3b151d38d123c1fe1407b0da70
Kevin Depolli <kevindepollidcf@gmail.com>
2023-09-17 16:40:25 -0300

    Rectify CalculadoraTaxaEntregaPromocional name

---
+++ src/main/java/com/ufes/CalculadoraTaxaEntregaPromocional.java
@@ -0,0 +1,34 @@
+package com.ufes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kevin
+ */
+public class CalculadoraTaxaEntregaPromocional implements ICalculadoraTaxaEntrega{
+    private double fatorPromocional;
+
+    public CalculadoraTaxaEntregaPromocional(double fatorPromocional) {
+        this.fatorPromocional = fatorPromocional;
+    }
+
+    public double getFatorPromocional() {
+        return fatorPromocional;
+    }
+
+    public void setFatorPromocional(double fatorPromocional) {
+        this.fatorPromocional = fatorPromocional;
+    }
+
+    @Override
+    public void calcularTaxaEntrega(Pedido pedido)
+    {
+        if(pedido.isPromocao())
+            pedido.setTaxaEntrega(fatorPromocional);
+    }
+    
+}
