8670a13eeced7ca88650c7fe5b31ab332a79f80a
Kevin Depolli <kevindepollidcf@gmail.com>
2023-09-17 16:49:18 -0300

    Create a ICalculadoraTaxaEntrega implementation CalculadoraTaxaEntregaDistancia

---
+++ src/main/java/com/ufes/CalculadoraTaxaEntregaDistancia.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ufes;
+
+/**
+ *
+ * @author Kevin
+ */
+public class CalculadoraTaxaEntregaDistancia implements ICalculadoraTaxaEntrega{
+    private double fatorDistancia;
+    private float distanciaMinima;
+
+    public CalculadoraTaxaEntregaDistancia(double fatorDistancia) {
+        this.fatorDistancia = fatorDistancia;
+    }
+
+    public double getFatorDistancia() {
+        return fatorDistancia;
+    }
+
+    public void setFatorDistancia(double fatorDistancia) {
+        this.fatorDistancia = fatorDistancia;
+    }
+    
+    @Override
+    public void calcularTaxaEntrega(Pedido pedido){
+        if(pedido.getDistancia() >= distanciaMinima)
+            pedido.setTaxaEntrega(distanciaMinima * fatorDistancia); 
+    }
+    
+}
